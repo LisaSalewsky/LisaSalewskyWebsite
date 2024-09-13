552942a75602306a73f3ab5447cec48f2e919586
DESKTOP-951NAKD\Lisa Salewsky <lisa.salewsky@gmx.de>
2024-09-13 17:48:30 +0200

    try fixing circle bars for ios

--- src/scripts/animate-elements.js
+++ src/scripts/animate-elements.js
@@ -14,6 +14,27 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const allAnimatedElements = document.querySelectorAll('.animate');
 
+    // const moreAnimatedElements = document.querySelectorAll('.radial-bar');
+
+    allAnimatedElements.forEach((element) => observer.observe(element));
+    // moreAnimatedElements.forEach((element) => observer.observe(element));
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+                return;
+            }
+            entry.target.classList.remove('in-view');
+        }); 
+    });
+
+    // const allAnimatedElements = document.querySelectorAll('.animate');
+
     const moreAnimatedElements = document.querySelectorAll('.radial-bar');
 
     // allAnimatedElements.forEach((element) => observer.observe(element));
